54e1580187fec28376a55e13809e5a67006b7cb2
thetechguy23 <tobiogunlesi95@gmail.com>
2023-02-19 07:41:26 -0800

    Print Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print alphabet in lowercase,  followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+		for (i = 0, i < 26; i++)
+		{
+			putchar(alp[i]);
+		}
+		putchar('\n');
+		return (0);
+}
