f3d8d62d0271a3575cccc35fc94983394d55f3d3
thetechguy23 <tobiogunlesi95@gmail.com>
2023-02-19 17:22:51 -0800

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,9 +3,9 @@
 /**
  * main - prints the number since 0 to 9
  *
- * Retutn: ALways (Success)
+ * 10 starting from 0, followed by a new line
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char n;
